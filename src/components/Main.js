0af241128c23d4a93fc619c309429acd4a058187
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-02-14 07:22:22 +0900

    Chat app for Qin LT

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import MessageInputField from './MessageInputField';
+import MessageList from './MessageList';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateRows: '1fr auto',
+    background: 'rgb(1,34,3)',
+    background: 'radial-gradient(circle, #56573c 3%, #190609 100%)',
+    color: '#fff',
+  },
+});
+
+const Main = ({ name }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <MessageList />
+      <MessageInputField name={name} />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/MessageItem.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from 'react';
+import Linkify from 'react-linkify'
+import {
+  Avatar,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { gravatarPath } from '../gravatar';
+
+const useStyles = makeStyles(() => ({
+  inline: {
+    display: 'inline',
+    color: '#fff',
+  },
+}));
+
+const MessageItem = ({ isLastItem, name, text }) => {
+  const ref = useRef(null);
+  const classes = useStyles();
+  const avatarPath = gravatarPath(name);
+
+  useEffect(() => {
+    if (isLastItem) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [isLastItem]);
+
+  return (
+    <ListItem divider={true} ref={ref}>
+      <ListItemAvatar>
+        <Avatar src={avatarPath} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={name}
+        secondary={
+          <Typography
+            component="span"
+            variant="body2"
+            className={classes.inline}
+            color="textPrimary"
+          >
+            <Linkify>{text}</Linkify>
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+};
+
+export default MessageItem;
