5f1e5d5a83a550ebff7df83b9eaa7c10224e880a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-06-20 14:25:38 +0900

    ✨ react-sparkesの導入

--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { IconButton } from '@material-ui/core';
-import SendIcon from '@material-ui/icons/Send';
+import Sparkles from "react-sparkle";
 
 import { pushMessage } from '../firebase';
 
 const MessageSubmitButton = ({ inputEl, name, setText, text, IsChecked }) => {
   return (
+    <div style={{ position: "relative" }}>
+      <Sparkles
+        color='#CEF9DC'
+        count={20}
+        minSize={7}
+        maxSize={12}
+        overflowPx={30}
+        fadeOutSpeed={30}
+        flicker={false}
+      />
+
     <IconButton
       style={{ color: "#0a016c" }}
       disabled={text === ""}
