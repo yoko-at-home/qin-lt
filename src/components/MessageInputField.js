58b883815689e6b99a45b853972aa3a966f07e58
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-06-20 14:26:06 +0900

    💄 背景色変更

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -12,9 +12,12 @@ const useStyles = makeStyles({
     gridRow: 2,
     margin: "26px",
     backgroundColor: "#e7f5ed",
+    backgroundImage:
+      "linear-gradient(to right, #f5f7f7, #f1f4f4, #edf1f2, #eaedef, #0C5259);",
     borderRadius: "5px",
     padding: "1rem",
     color: "#fff",
+    boxShadow: "0px 0px 12px 1px #f0f0f0",
   },
 });
 
